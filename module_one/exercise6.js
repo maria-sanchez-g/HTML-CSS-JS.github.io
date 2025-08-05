70c67e1ce3bf01e00faf2601750a423f23da67b1
maria-sanchez-g <marysg_92@hotmail.com>
2025-08-05 18:35:10 +1000

    last

--- module_one/exercise6.js
+++ module_one/exercise6.js
@@ -13,12 +13,18 @@ console.log(book.number_pages);
 book.description = "data";
 console.log(book.description);
 
-const library = [
-  { title: "Book 1", description: "Desc 1", author: "Author 1", number_pages: "120" },
-  { title: "Book 2", description: "Desc 2", author: "Author 2", number_pages: "150" },
-  { title: "Book 3", description: "Desc 3", author: "Author 3", number_pages: "200" },
-  { title: "Book 4", description: "Desc 4", author: "Author 4", number_pages: "180" },
-  { title: "Book 5", description: "Desc 5", author: "Author 5", number_pages: "300" }
-];
+const Book1 = [
+  { title: "Book 1", description: "Desc 1", author: "Author 1", number_pages: "120" }];
+const Book2 = [
+  { title: "Book 2", description: "Desc 2", author: "Author 2", number_pages: "150" }];
+const Book3 = [
+  { title: "Book 3", description: "Desc 3", author: "Author 3", number_pages: "200" }];
+const Book4 = [
+  { title: "Book 4", description: "Desc 4", author: "Author 4", number_pages: "180" }];
+const Book5 = [
+  { title: "Book 5", description: "Desc 5", author: "Author 5", number_pages: "300" }];
+
+
+const library = [Book1, Book2, Book3, Book4, Book5]
 
 console.log("Library:", library);
