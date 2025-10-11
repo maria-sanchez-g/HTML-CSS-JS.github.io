e3c38eade5f53a403b4499e81dd6ef3162a9c882
maria-sanchez-g <marysg_92@hotmail.com>
2025-10-11 15:18:29 +1100

    last

--- Classes/Module_7/first-next-app/src/app/page.js
+++ Classes/Module_7/first-next-app/src/app/page.js
@@ -15,40 +15,12 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+           this is index page
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
-<<<<<<< HEAD
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-=======
 
->>>>>>> 8bc4e7d2323faa128e9da498be3a3b836d4eba98
       </main>
       <footer className={styles.footer}>
         <a
