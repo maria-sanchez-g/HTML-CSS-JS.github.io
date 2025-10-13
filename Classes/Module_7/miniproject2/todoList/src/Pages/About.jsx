df213620609f3dc7a7163cef6a2f3d2f87f95506
maria-sanchez-g <marysg_92@hotmail.com>
2025-10-13 21:06:36 +1100

    last

---
+++ Classes/Module_7/miniproject2/todoList/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+export default function About() {
+  return (
+    <>
+      <Grid container>
+        <Grid offset={3}>
+          <div>This is about page</div>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
