44da0b398f5e793b97bda458adc7860b00de5faa
maria-sanchez-g <marysg_92@hotmail.com>
2025-10-26 13:43:49 +1100

    last

---
+++ React/8_Emoji_Context/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Emoji from './Components/Emoji'
+import './App.css'
+
+function App() {
+  return (
+    <>
+<Emoji></Emoji>
+    </>
+  )
+}
+
+export default App
+
