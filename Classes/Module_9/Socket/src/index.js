1c4de66e9737982da26b0a9d1e50866e29248831
maria-sanchez-g <marysg_92@hotmail.com>
2025-11-05 19:18:59 +1100

    last

--- Classes/Module_9/Socket/src/index.js
+++ Classes/Module_9/Socket/src/index.js
@@ -1,16 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-io.on('connection', (socket) => {
-    console.log("socket.id", socket.id)
-    io.emit('connection', 'a user connected');
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("msg", msg);
+    io.emit("boardcast message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
